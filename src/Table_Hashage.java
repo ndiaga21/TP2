0b1ac62d57531227f9d2a2a75bc4eb13b3cf2dfb
ndiaga21 <ndiagadiengs1@gmail.com>
2020-03-02 18:58:18 +0100

    cool

--- src/Table_Hashage.java
+++ src/Table_Hashage.java
@@ -1,34 +1,38 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Table_Hashage {
      final static int TAILLE_HASHMAP = 17;
     static HashMap<Integer, LinkedList<ArrayList<Character>>> hashMap = new HashMap<>();
-     static LinkedList<ArrayList<Character>> list;
+
      public static void recupere_mots_dico() throws IOException {
-          list = new LinkedList<>();
           for (String mot:Lecture_Dictionnaire.getLines()) {
                Multi_Set multi_set= new Multi_Set(mot);
                Multi_Set.getEnsemble_R().sort(Character::compareTo);
-              // System.out.println(Multi_Set.getEnsemble_R()+" "+multi_set.hashCode());
-                list.addAll(chainage_externe(multi_set));
+                hashMap.put(multi_set.hashCode(),chainage_externe(multi_set));
 
           }
-          //hashMap.put(new Multi_Set(list.get(0).toString()).hashCode(),list);
 
-          System.out.println(list);
+
+        System.out.println(hashMap);
      }
 
-     private static LinkedList<ArrayList<Character>> chainage_externe(Multi_Set multi_set) {
+     private static LinkedList<ArrayList<Character>> chainage_externe(Multi_Set multi_set) throws IOException {
            LinkedList<ArrayList<Character>> list = new LinkedList<>();
-         // System.out.println(Multi_Set.getEnsemble_R());
-          if (multi_set.hashCode() == 336 ) {
+           TreeSet<ArrayList<Character>> set = new TreeSet<>();
+           for (String mot : Lecture_Dictionnaire.getLines()) {
 
-               list.add(Multi_Set.getEnsemble_R());
-               
+               if (multi_set.hashCode() == new Multi_Set(mot).hashCode() ) {
+                    Multi_Set.getEnsemble_R().sort(Character::compareTo);
+                   // System.out.println(Multi_Set.getEnsemble_R());
+                    if (!list.contains(Multi_Set.getEnsemble_R()))
+
+                    list.add(Multi_Set.getEnsemble_R());
+               }
+
           }
+
+
 
           return list;
      }
